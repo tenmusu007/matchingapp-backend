5aacc7fa6d94af5868eeeb105e3aaf0a016087ab
Atsuya Tanaka <tenmusu007@gmail.com>
2022-11-25 15:45:24 -0800

    fix cookiesession

--- index.js
+++ index.js
@@ -1,10 +1,50 @@
 const express = require("express");
 const app = express();
-const port = 8000;
-const cookieSession = require("cookie-session");
 const cors = require("cors");
-const { Server } = require("socket.io");
+const cookieSession = require("cookie-session");
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+const port = 8000;
+require("dotenv").config();
 const http = require("http");
+app.use(
+	cors({
+		origin: "http://localhost:3000",
+		credentials: true,
+	})
+);
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server, {
+	cors: {
+		origin: "http://localhost:3000",
+		methods: ["GET", "POST", "DELETE"],
+	},
+});
+app.use(
+	cookieSession({
+		name: "id",
+		secret: "key",
+		resave: true,
+		saveUninitialized: false,
+		cookie: {
+			maxAge: 10000 * 60 * 60,
+			secure: false,
+		},
+	})
+);
+
+io.on("connection", async (socket) => {
+	socket.on("join_room", (roomId) => {
+		socket.join(roomId);
+		io.to(roomId).emit("joined_room", roomId);
+	});
+	socket.on("send_msg", (data) => {
+		console.log("msg", data);
+		io.to(data.roomId).emit("recived_msg", data.data);
+	});
+	socket.on("disconnect", () => {});
+});
 const mongoose = require("mongoose");
 const authRoute = require("./routes/auth");
 const settingRoute = require("./routes/setting");
@@ -20,43 +60,14 @@ const sharp = require("sharp");
 const path = require("path");
 const {
 	S3Client,
+	PutObjectCommand,
+	GetObjectCommand,
 	DeleteBucketCommand,
 } = require("@aws-sdk/client-s3");
+// const  { getSignedUrl } = require("@aws-sdk/s3-request-presigner");
 const Images = require("./models/Images");
-require("dotenv").config();
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-app.use(
-	cookieSession({
-		name: "session",
-		keys:["key1","key2"]
-	})
-	);
-	app.use("/", authRoute);
-	app.use("/", settingRoute);
-	app.use("/", interestsRoute);
-	app.use("/", likesRoute);
-	app.use("/", chatRoute);
-	app.use("/", imageRoute);
-	const server = http.createServer(app);
-const io = new Server(server, {
-	cors: {
-		origin: "http://localhost:3000",
-		methods: ["GET", "POST", "DELETE"],
-	},
-});
-
-io.on("connection", async (socket) => {
-	socket.on("join_room", (roomId) => {
-		socket.join(roomId);
-		io.to(roomId).emit("joined_room", roomId);
-	});
-	socket.on("send_msg", (data) => {
-		console.log("msg", data);
-		io.to(data.roomId).emit("recived_msg", data.data);
-	});
-	socket.on("disconnect", () => {});
-});
+const randomImageName = (bytes = 32) =>
+	crypto.randomBytes(bytes).toString("hex");
 const bucketName = process.env.BUCKET_NAME;
 const bucketRegion = process.env.BUCKET_REGION;
 const accessKey = process.env.ACCESS_KEY;
@@ -76,6 +87,38 @@ mongoose
 	.catch((err) => {
 		console.log(err);
 	});
+
+// const __dirname = path.resolve();
+
+// if (process.env.NODE_ENV === "production") {
+// 	app.use(express.static(path.join(__dirname, "/frontend/build")));
+
+// 	app.get("*", (req, res) =>
+// 		res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
+// 	);
+// } else {
+// 	app.get("/", (req, res) => {
+// 		res.send("API is running..");
+// 	});
+// }
+app.use("/", authRoute);
+
+app.use("/", settingRoute);
+app.use("/", interestsRoute);
+app.use("/", likesRoute);
+app.use("/", chatRoute);
+app.use("/", imageRoute);
+app.post("/deleteimage", async (req, res) => {
+	const image = await Images.findOne({ usr_id: "6364005204c4d5b81220fe46" });
+	const getObjectParams = {
+		Bucket: bucketName,
+		Key: image.path,
+	};
+	const command = new DeleteBucketCommand(getObjectParams);
+	await s3.send(command);
+	await Pictuers.deleteOne({ path: image.path });
+	res.send("ok");
+});
 server.listen(process.env.PORT|| port, () => {
 	console.log(`listening on port ${port}`);
 });
