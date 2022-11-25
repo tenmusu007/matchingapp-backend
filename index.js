78b386c06284e08239eb45576045af6429588f90
Atsuya Tanaka <tenmusu007@gmail.com>
2022-11-25 05:36:07 -0800

    make it better

--- index.js
+++ index.js
@@ -84,17 +84,17 @@ mongoose
 
 // const __dirname = path.resolve();
 
-if (process.env.NODE_ENV === "production") {
-	app.use(express.static(path.join(__dirname, "/frontend/build")));
+// if (process.env.NODE_ENV === "production") {
+// 	app.use(express.static(path.join(__dirname, "/frontend/build")));
 
-	app.get("*", (req, res) =>
-		res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
-	);
-} else {
-	app.get("/", (req, res) => {
-		res.send("API is running..");
-	});
-}
+// 	app.get("*", (req, res) =>
+// 		res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
+// 	);
+// } else {
+// 	app.get("/", (req, res) => {
+// 		res.send("API is running..");
+// 	});
+// }
 app.use("/", authRoute);
 
 app.use("/", settingRoute);
