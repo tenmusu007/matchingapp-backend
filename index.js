b01515c235dce390d32f587d09ed2caf39c08f8e
Atsuya Tanaka <tenmusu007@gmail.com>
2022-11-27 07:02:38 -0800

    Add allowedHeader

--- index.js
+++ index.js
@@ -30,13 +30,11 @@ app.use("/", imageRoute);
 const server = http.createServer(app);
 const io = new Server(server, {
 	cors: {
-		origin: ["http://localhost:3000", process.env.FRONT_URL],
+		origin: process.env.FRONT_URL,
 		methods: ["GET", "POST", "DELETE"],
-		credentials :true
+		allowedHeaders: ["my-custom-header"],
+		credentials: true,
 	},
-});
-io.engine.on("headers", (headers, req) => {
-	headers["Access-Control-Allow-Origin"] = process.env.FRONT_URL; 
 });
 
 io.on("connection", async (socket) => {
