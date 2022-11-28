eeca8aead8fabe44ccb54fa4c499078601b90e7f
Atsuya Tanaka <tenmusu007@gmail.com>
2022-11-28 10:16:18 -0800

    delete path

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ app.use(
 		credentials: true,
 	})
 );
-app.use("/api", authRoute);
+app.use("/", authRoute);
 app.use("/", settingRoute);
 app.use("/", interestsRoute);
 app.use("/", likesRoute);
