3f537906850df26f532d8f8ddf0f9fb749d7e4e6
Atsuya Tanaka <tenmusu007@gmail.com>
2022-12-01 10:01:44 -0800

    add addImageUrl file and func for image at home

--- helper/addImageUrl.js
+++ helper/addImageUrl.js
@@ -1,34 +1,34 @@
 const Images = require("../models/Images");
 const { getImageFromS3 } = require("./getImageFromS3");
 
-
 const getImageForChatList = async (userData) => {
 	const image = await Images.findOne({
 		user_id: userData._id.toString(),
 	});
 	if (image) {
-    const url = await getImageFromS3(image.path);
-    userData.image = url
-    return userData
+		const url = await getImageFromS3(image.path);
+		userData.image = url;
+		return userData;
 	} else {
-    return userData
+		return userData;
 	}
 };
 const getImageForHome = async (userData) => {
-  const getImageForUserList = userData.map(async(item) => {
+	for (const item of userData) {
     const image = await Images.findOne({
       user_id: item._id.toString(),
-    });
-    if (image) {
+		});
+		if (image) {
       const url = await getImageFromS3(image.path);
-      userData.image = url
-      return userData
-    } else {
-      userData.image = "nothing"
-      return userData
-    }
-  })
-  console.log("getImageForUse rList",getImageForUserList);
-  return getImageForUserList;
-}
+			userData.image = url;
+			// return userData;
+		} else {
+      userData.image = "nothing";
+			// return userData;
+		}
+  }
+	console.log("getImageForUse rList", getImageForUserList);
+	// return getImageForUserList;
+  return userData
+};
 module.exports = { getImageForChatList, getImageForHome };
